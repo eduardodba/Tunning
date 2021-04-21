6b591196aea0faeca324580f941b7e1aaea29a1d
eduardodba <57985139+eduardodba@users.noreply.github.com>
2021-04-21 20:23:51 -0300

    Update update.sql

--- update.sql
+++ update.sql
@@ -10,9 +10,9 @@ UPDATE CustomersTest SET contactName = NULL
 
 
 
--- Custo 112.8 / Memory Grant 532 MB / Tempo de execução 2 min 20 seg
+-- Custo 112.8 / Memory Grant 548 MB / Tempo de execução 3 min 08 seg
 
-UPDATE t SET t.contactName = CASE WHEN m.contactName NOT IN (
+UPDATE t SET t.contactName = CASE WHEN m.contactName IN (
 'Roland Mendel 15D2D14C'
 ,'Francisco Chang 85F9DB01'
 ,'Horst Kloss 551D67ED'
@@ -26,7 +26,7 @@ FROM CustomersTest t INNER JOIN CustomersModelTest m ON t.CustomerID=m.CustomerI
 
 
 
--- Custo 12 / Memory Grant 12 MB / Tempo de execução 01 seg 
+-- Custo 12 / Memory Grant 16 MB / Tempo de execução 01 seg 
 
 DECLARE @tab TABLE(CustomerID INT, contactName VARCHAR(209))
 INSERT INTO @tab (customerID,contactName)
@@ -41,5 +41,7 @@ SELECT customerID, contactName FROM CustomersModelTest WHERE contactName IN (
 ,'Fran Wilson 814575BD') 
 
 
-UPDATE t SET t.contactName = (CASE WHEN t.customerID = m.customerID THEN m.contactName ELSE 'SEM NOME' END)
+UPDATE t SET t.contactName = (CASE WHEN t.customerID = m.customerID THEN 'SEM NOME' ELSE m.contactName END)
 FROM CustomersTest t INNER JOIN @tab m ON t.customerID=m.customerID
+
+
